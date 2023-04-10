4bf8663d50abcdf4c33cc3664303773d6456ed57
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2023-04-10 10:59:49 -0500

    Update CreateMailboxes.ps1

--- CreateMailboxes.ps1
+++ CreateMailboxes.ps1
@@ -1,14 +1,14 @@
 ﻿
-# Need to add Powershell Exchange Snap in to the shell.
-#  Run the following in the terminal First.  Add-PSSnapin -name Microsoft.Exchange.Management.Powershell.SnapIn
+# This script will enable/add exchangemailboxes for all the users in the AD.
 # Grab user data
 $UserImport = Import-CSV C:\Users\Administrator\Desktop\SetupScripts\GFUsers.csv
-$OUpath = "OU=Users,OU=sis-Accounts,DC=galfed,DC=com"
+# Change the below variable to match the OU where you're users live.
+$OUpath = "OU=Users,OU=GF-Accounts,DC=galfed,DC=com"
 # Loop through users and create accounts and set group memberships.
+Add-PSSnapin -name Microsoft.Exchange.Management.Powershell.SnapIn
 $UserImport | ForEach-Object {
   $alias = $_.FirstName + "." + $_.LastName
 
   Enable-Mailbox -Identity $alias -Database sis-maildb
 
 }
-
