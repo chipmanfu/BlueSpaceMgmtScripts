89ea122649164c07bdc90bd83b583202163cea22
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2023-04-10 10:50:07 -0500

    Add files via upload

---
+++ CreateADUsers2.ps1
@@ -0,0 +1,80 @@
+﻿# Create destination AD Structures
+New-ADOrganizationalUnit -name "GF-Accounts" -path "DC=galfed,DC=com" 
+New-ADOrganizationalUnit -name "Users" -path "OU=GF-Accounts,DC=galfed,DC=com"
+New-ADOrganizationalUnit -name "Admins" -path "OU=GF-Accounts,DC=galfed,DC=com"
+New-ADOrganizationalUnit -name "GF-Groups" -path "DC=galfed,DC=com"
+
+# Set custom parent AD Paths variables
+$useracctpath = "OU=Users,OU=GF-Accounts,DC=galfed,DC=com"
+$adminacctpath = "OU=Admins,OU=GF-Accounts,DC=galfed,DC=com"
+$grouphomepath = "OU=GF-Groups,DC=galfed,DC=com"
+
+# Creatr catchall group for Similar occupations
+New-ADGroup -Name "IT support" -GroupScope Global -Path $grouphomepath
+New-ADGroup -Name "Executive" -GroupScope Global -Path $grouphomepath
+
+# Grab user data
+$UserImport = Import-CSV c:\Users\Administrator\Desktop\SetupScripts\GFUsers.csv
+
+# Loop through users and create accounts and set group memberships.
+$UserImport | ForEach-Object {
+  $givenname = $_.Firstname
+  $initial = $_.MiddleInitial 
+  $surname = $_.LastName
+  $fullname = $_.FirstName + " " + $_.MiddleInitial + " " + $_.LastName
+  $samname = $_.FirstName + "." + $_.LastName
+  $email = $samname + "@galfed.com"
+  $pass = $_.LastName + "Pass"
+  $password = (ConvertTo-SecureString $pass -AsPlainText -Force)
+  $group = $_.Occupation
+
+  New-ADuser `
+   -GivenName $givenname `
+   -Initials $initial `
+   -Surname $surname `
+   -Name $fullname `
+   -Path $useracctpath `
+   -SamAccountName $samname `
+   -EmailAddress $email `
+   -AccountPassword $password `
+   -ChangePasswordAtLogon $false `
+   -PasswordNeverExpires $true `
+   -Enabled $true `
+   -Office $group `
+   -Company "Galactic Federation" `
+   -DisplayName $fullname `
+   -Verbose 
+
+# Check if System Admin, if so create second account for domain admin use.
+  if ($_.Occupation -eq "Quantium Tech"){
+    # Create Domain Admin account - format First.Last.admin
+    $adminsam = $samname + ".adm"
+    $newpass = $_.LastName + "Admin"
+    $adminpass = (ConvertTo-SecureString $newpass -AsPlainText -Force)
+    New-ADuser `
+     -Name $fullname `
+     -Path $adminacctpath `
+     -SamAccountName $adminsam `
+     -AccountPassword $adminpass `
+     -ChangePasswordAtLogon $false `
+     -PasswordNeverExpires $true `
+     -Enabled $true `
+     -Description $group `
+     -Company "Galactic Federation" `
+     -DisplayName $fullname `
+     -Verbose 
+    Add-ADGroupMember "Domain Admins" $adminsam -Verbose
+    Add-ADGroupMember "Enterprise Admins" $adminsam -Verbose
+    Add-ADGroupMember "IT support" $samname -Verbose
+  } 
+  ElseIf ($_.Occupation -like "Galactic Ambassador"){
+    Add-ADGroupMember "Executive" $samname -Verbose
+  }
+  ElseIf (-not (Get-ADGroup -Filter "Name -eq '$group'" -ErrorAction SilentlyContinue)){
+    New-ADGroup -Name "$group" -GroupScope Global -Path $grouphomepath
+    Add-ADGroupMember "$group" $samname -Verbose
+  } else {
+    Add-ADgroupMember "$group" $samname -Verbose
+  }
+}
+
---
+++ CreateMailboxes.ps1
@@ -0,0 +1,14 @@
+﻿
+# Need to add Powershell Exchange Snap in to the shell.
+#  Run the following in the terminal First.  Add-PSSnapin -name Microsoft.Exchange.Management.Powershell.SnapIn
+# Grab user data
+$UserImport = Import-CSV C:\Users\Administrator\Desktop\SetupScripts\GFUsers.csv
+$OUpath = "OU=Users,OU=sis-Accounts,DC=galfed,DC=com"
+# Loop through users and create accounts and set group memberships.
+$UserImport | ForEach-Object {
+  $alias = $_.FirstName + "." + $_.LastName
+
+  Enable-Mailbox -Identity $alias -Database sis-maildb
+
+}
+
